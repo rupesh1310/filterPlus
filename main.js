cbefdacb702bceb7e47e72c339bf314f090e7dd8
A.Rupesh <a.rupesh66@gmail.com>
2018-09-22 01:53:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.msGetInputContext('2d');
+
+let img = new Image();
+let fileName = '';
+
+const downloadBtn = document.getElementById('download-btn');
+const downloadBtn = document.getElementById('upload-file');
+const downloadBtn = document.getElementById('revert-btn');
